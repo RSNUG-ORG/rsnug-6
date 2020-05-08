87d78b4dbd509df97771160d41d8ba30b8e9fa2d
Gabriel Schmitt Kohlrausch <gabriel@society.com.br>
2020-05-07 23:54:35 -0300

    Adicionado metodo de extensão para calcular idade

---
+++ Exemplos/EF-Core-Base/DDDTalk.Dominio/Shared/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetAge(this DateTime dataNascimento, DateTime quando)
+        {
+            if (quando.Month < dataNascimento.Month ||
+                    quando.Month == dataNascimento.Month &&
+                    quando.Day < dataNascimento.Day)            
+                return quando.Year - dataNascimento.Year - 1;                        
+            return quando.Year - dataNascimento.Year;
+        }
+    }
+}
